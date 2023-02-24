7000fb21501f032c3b9650fa005bbcba6bfa6e9e
Aaron MV <epsilon11101@gmail.com>
2023-02-24 17:43:39 -0600

    feat: add totalMax expeses

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,13 +3,16 @@ import Bar from "./Bar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <Bar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
